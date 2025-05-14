579f14f5b68e48f13b65c68f7c43713aaf20a299
mdco-ld <migueldco14@gmail.com>
2025-05-14 18:29:47 -0300

    update sieve

--- sieve.cpp
+++ sieve.cpp
@@ -1,20 +1,21 @@
 #include <vector>
+#include <numeric>
 using namespace std;
+
 struct Sieve {
-    vector<int> spf, pr;
-    
-    Sieve(int n) : spf(n + 1) {
-        for (int i = 2; i <= n; i++) {
-            if (!spf[i]) {
-                spf[i] = i;
-                pr.push_back(i);
-            }
-            for (int p : pr) {
-                if (p > spf[i] || i * p > n) break;
-                spf[i * p] = p;
-            }
-        }
-    }
-    
-    bool is_prime(int x) { return spf[x] == x; }
+	vector<int> spf, pr;
+	Sieve(int n): spf(n) {
+		iota(spf.begin(), spf.end(), 0);
+		for (int i = 2; i < n; i++) {
+			if (spf[i] == i) {
+				pr.push_back(i);
+			}
+			for (int j = 0; pr[j] * i < n && j < pr.size(); j++) {
+				spf[pr[j] * i] = pr[j];
+				if (pr[j] == spf[i]) {
+					break;
+				}
+			}
+		}
+	}
 };
